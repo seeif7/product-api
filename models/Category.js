f2af83ac103ca9cfbeac3b89ee36527aaf6b8101
Seif <your_email@example.com>
2026-07-04 16:16:23 +0300

    feat: implement category model, controller, and routes

---
+++ models/Category.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const CategorySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a category name'],
+        unique: true,
+        trim: true
+    },
+    description: {
+        type: String,
+        required: [true, 'Please add a description']
+    }
+}, { timestamps: true });
+
+module.exports = mongoose.model('Category', CategorySchema);
